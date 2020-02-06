c19690532a51b57f502d4d737f3fb15ba2b6de74
Patrik Kenyeres <kenyeres.patrik99@hotmail.com>
2020-02-06 21:24:39 +0100

    Na így talán

---
+++ próba megint/próba megint/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace próba_megint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                Random gép = new Random();
+                char[,] pálya = new char[10, 10];
+                int sor = 0, oszlop = 0;
+                feltöltő(pálya);
+                kirajzoló(pálya);
+
+                Console.ReadKey();
+
+            }
+
+            private static void kirajzoló(char[,] pálya)
+            {
+                for (int sor = 0; sor < pálya.GetLength(0); sor++)
+                {
+                    for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                    {
+                        Console.WriteLine($"{pálya[sor, oszlop]}|");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            private static void feltöltő(char[,] pálya)
+            {
+                for (int sor = 0; sor < pálya.GetLength(0); sor++)
+                {
+                    for (int oszlop = 0; oszlop < pálya.GetLength(1); oszlop++)
+                    {
+                        Console.WriteLine(pálya[sor, oszlop] = '_');
+                    }
+
+                }
+            }
+        }
+    }
+}
